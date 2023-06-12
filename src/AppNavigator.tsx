81466b6db982a8d41bc94f0b649d77bfa460743a
Norman Chia <norman.chia.minjie@gmail.com>
2023-06-13 03:18:24 +0800

    chore/remove useless file

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -2,17 +2,7 @@ import React from 'react';
 import { StatusBar, StyleSheet, useWindowDimensions } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {
-  HomeScene,
-  DrawerContent,
-  HelpScene,
-  FeedbackScene,
-  InviteFriendScene,
-} from '.';
-import { CourseInfoScreen, HomeDesignCourse } from './design_course';
-import { IntroductionAnimationScreen } from './introduction_animation';
-import HotelHomeScreen from './hotel_booking/HotelHomeScreen';
+
 import {
   DashboardScreen,
   AddAppointmentScreen,
@@ -32,37 +22,6 @@ const Drawer = createDrawerNavigator();
  */
 const Stack = createStackNavigator();
 // const Stack = createNativeStackNavigator();
-
-const DrawerNavigator: React.FC = () => {
-  const window = useWindowDimensions();
-
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        drawerStyle: {
-          width: window.width * 0.75,
-          backgroundColor: 'rgb(237, 240, 242, 0.5)',
-        },
-        sceneContainerStyle: styles.drawerSceneContainer,
-        drawerActiveBackgroundColor: '#5cbbff',
-        drawerType: 'back',
-        overlayColor: 'transparent',
-        swipeEdgeWidth: window.width,
-        headerShown: false,
-      }}
-      drawerContent={props => <DrawerContent {...props} />}
-      // this is just to enable shadow/elevation style on drawer, as it fallback to JS drawer solution instead of native one (v6)
-      // as explained here:- https://github.com/react-navigation/react-navigation/issues/10946#issuecomment-1287082343
-      detachInactiveScreens={false}
-    >
-      <Drawer.Screen name="home" component={HomeScene} />
-      <Drawer.Screen name="help" component={HelpScene} />
-      <Drawer.Screen name="feedback" component={FeedbackScene} />
-      <Drawer.Screen name="invite_friend" component={InviteFriendScene} />
-      <Drawer.Screen name="DashboardScreen" component={DashboardScreen} />
-    </Drawer.Navigator>
-  );
-};
 
 export default () => {
   return (
