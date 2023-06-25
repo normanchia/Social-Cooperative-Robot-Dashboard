5949533f3594c5a3e4072f7b45695883e36aed0d
Fabian Chua <fabianchua6@gmail.com>
2023-06-25 22:14:28 +0800

    clean up setting screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,42 +1,96 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   View,
   Text,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
-  Dimensions,
 } from 'react-native';
 
 import { mainContainer, bodyContainer } from '../styles/styles';
 import Header from '../components/Header';
 import { useTheme } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet } from 'react-native';
+
+const ICON_SIZE = 60;
 
 const settingsOptions = [
-  { text: 'Account Information', iconName: 'user' },
-  { text: 'Preferences', iconName: 'cog' },
-  { text: 'Notifications', iconName: 'bell' },
-  // { text: 'Help & Support', iconName: 'question-circle' },
-  { text: 'Logout', iconName: 'sign-out' },
+  {
+    text: 'Account Information',
+    iconName: 'account-circle',
+    color: '#6495ed',
+    textColor: '#000',
+  },
+  {
+    text: 'Preferences',
+    iconName: 'tune',
+    color: '#3cb371',
+    textColor: '#000',
+  },
+  {
+    text: 'Toggle Dark Mode',
+    iconName: 'brightness-6',
+    color: '#9c9c9c',
+    textColor: '#000',
+  },
+  {
+    text: 'Notifications',
+    iconName: 'notifications',
+    color: '#800080',
+    textColor: '#000',
+  },
+  {
+    text: 'Logout',
+    iconName: 'exit-to-app',
+    color: '#ff7f7f',
+    textColor: '#000',
+  },
 ];
 
-const numColumns = 2;
-const size = Dimensions.get('window').width / numColumns;
+const SettingItem = ({ item, toggleSwitch, isDarkMode }) => {
+  const theme = useTheme();
+  const buttonStyle = {
+    ...styles.button,
+    backgroundColor: theme.colors.surface,
+  };
+  const textStyle = { ...styles.text, color: item.textColor };
 
-const SettingsScreen: React.FC = () => {
-  const theme = useTheme(); // use the theme hook
-  const renderItem = ({ item }) => (
+  return (
     <TouchableOpacity
-      style={{
-        width: size,
-        height: size,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+      style={buttonStyle}
+      onPress={item.text === 'Toggle Dark Mode' ? toggleSwitch : undefined}
     >
-      <Text>{item.text}</Text>
+      <Icon name={item.iconName} size={ICON_SIZE} color={item.textColor} />
+      <Text style={textStyle}>
+        {item.text === 'Toggle Dark Mode'
+          ? `Toggle ${isDarkMode ? 'Light' : 'Dark'} Mode`
+          : item.text}
+      </Text>
     </TouchableOpacity>
   );
+};
+
+const SettingsScreen: React.FC = () => {
+  const theme = useTheme();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleSwitch = useCallback(
+    () => setIsDarkMode(previousState => !previousState),
+    [],
+  );
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <SettingItem
+        item={item}
+        toggleSwitch={toggleSwitch}
+        isDarkMode={isDarkMode}
+      />
+    ),
+    [toggleSwitch, isDarkMode],
+  );
+
   return (
     <>
       <SafeAreaView
@@ -53,13 +107,38 @@ const SettingsScreen: React.FC = () => {
             data={settingsOptions}
             renderItem={renderItem}
             keyExtractor={item => item.text}
-            numColumns={numColumns}
-            key={numColumns}
+            numColumns={1}
+            contentContainerStyle={{ flexGrow: 1 }} // updated line
           />
         </View>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    margin: 5,
+    marginBottom: 10,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, // for Android
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
 
 export default SettingsScreen;
