004ef22641e68044f47a008a3ea11165c5c08a20
Norman Chia <norman.chia.minjie@gmail.com>
2023-07-04 19:53:09 +0000

    fix/deploy needs

--- ITP_Backend/app.py
+++ ITP_Backend/app.py
@@ -31,4 +31,4 @@ app.register_blueprint(robot_request_blueprint)
 app.register_blueprint(driver_request_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
