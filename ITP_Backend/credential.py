81505221a1cf2095e00222dee2c82afc0775bcd4
Norman Chia <norman.chia.minjie@gmail.com>
2023-07-09 16:11:30 +0800

    remote branch testing

--- ITP_Backend/credential.py
+++ ITP_Backend/credential.py
@@ -1,4 +1,9 @@
-host = 'localhost'
+# host = 'localhost'
+# user = 'root'
+# password = ''
+# database = 'itp_team10_se'
+
+host = '34.87.19.215'
 user = 'root'
-password = ''
+password = 'tH15is@sup3rsTr0nGp@$$w0rd'
 database = 'itp_team10_se'
